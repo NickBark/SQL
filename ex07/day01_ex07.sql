fa5e0e735b9dc21e441d0165aa796da6d21eb119
Bober <thedillerthe@gmail.com>
2023-09-19 17:49:43 +0300

    fix

--- ex07/day01_ex07.sql
+++ ex07/day01_ex07.sql
@@ -1,6 +1,6 @@
 SELECT
     po.order_date AS order_date,
-    CONCAT(person.name, '(age:', person.age,')')
+    CONCAT(person.name, ' (age:', person.age,')')
         AS person_information
 FROM person_order AS po
 JOIN person ON po.person_id = person.id
--- ex08/day01_ex08.sql
+++ ex08/day01_ex08.sql
@@ -1,6 +1,6 @@
 SELECT
     person_order.order_date AS order_date,
-    CONCAT(person.name, '(age:', person.age,')')
+    CONCAT(person.name, ' (age:', person.age,')')
         AS person_information
 FROM person_order
 NATURAL JOIN 
